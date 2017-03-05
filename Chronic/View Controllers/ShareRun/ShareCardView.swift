a4c27c431f293be9616c92f9945a37ad09edb02c
Ace Green <acegreen1989@icloud.com>
2017-03-05 10:31:21 -0500

    initial share feature work

---
+++ Chronic/View Controllers/ShareRun/ShareCardView.swift
@@ -0,0 +1,38 @@
+//
+//  ShareCardView.swift
+//  Chronic
+//
+//  Created by Ace Green on 2017-01-09.
+//  Copyright © 2017 StockSwipe. All rights reserved.
+//
+
+import UIKit
+
+class ShareCardView: UIView {
+    
+    @IBOutlet weak var distanceValueLabel: UILabel!
+    @IBOutlet weak var distanceUnitLabel: UILabel!
+    @IBOutlet weak var distanceTitleLabel: UILabel!
+    
+    @IBOutlet weak var avgPaceValueLabel: UILabel!
+    @IBOutlet weak var avgPaceUnitLabel: UILabel!
+    @IBOutlet weak var avePaceTitleLabel: UILabel!
+    
+    @IBOutlet weak var totalTimeValueLabel: UILabel!
+    @IBOutlet weak var totalTimeTitleLabel: UILabel!
+    
+    @IBOutlet weak var rythmicValueLabel: UILabel!
+    @IBOutlet weak var rythmicTitleLabel: UILabel!
+    
+    @IBOutlet weak var hrValueLabel: UILabel!
+    @IBOutlet weak var hrUnitLabel: UILabel!
+    @IBOutlet weak var hrTitleLabel: UILabel!
+    
+    @IBOutlet weak var caloriesValueLabel: UILabel!
+    @IBOutlet weak var caloriesTitleLabel: UILabel!
+    
+    @IBOutlet weak var smartZoneView: UIView!
+        
+    @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var bottomStackViewSeparatorView: UIStackView!
+}
